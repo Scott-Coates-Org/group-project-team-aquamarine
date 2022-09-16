d402fe5c33be4bbdcb2ba8c7b4b12350880afa09
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2022-09-16 19:40:32 +0200

    sprint1 - homepage layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import CustomerPage from "./layouts/CustomerPage.jsx";
 import { Flex } from "@chakra-ui/react";
+import HomePage from "./layouts/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Flex justify="center" align="center" fontFamily="Ubuntu">
+        {/* <HomePage /> */}
         <CustomerPage />
       </Flex>
     </div>
